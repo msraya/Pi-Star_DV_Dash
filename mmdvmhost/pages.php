3e9661a3c2e14c01bc1d3323e616365052a816da
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2019-05-20 11:11:20 +0200

    Factorize the code.

--- mmdvmhost/pages.php
+++ mmdvmhost/pages.php
@@ -134,96 +134,83 @@ if (strcmp($origin, "admin") == 0) {
     // Display personnal messages only if RIC has been defined, and some personnal messages are available
     if ($myRIC && (array_search('<MY_RIC>', $logLinesDAPNETGateway) != FALSE)) {
 ?>
-    
-    <input type="hidden" name="pocsag-autorefresh" value="OFF" />
-
-    <!-- Personnal messages-->
-    
     <div>
-
-    <b><?php echo $lang['pocsag_persolist'];?></b>
-	
+	<input type="hidden" name="pocsag-autorefresh" value="OFF" />
+	<!-- Personnal messages-->
 	<div>
-	    <table>
-		<thread>
-		    <tr>
-			<th style="width: 140px;" ><a class="tooltip" href="#"><?php echo $lang['time'];?> (<?php echo date('T')?>)<span><b>Time in <?php echo date('T')?> time zone</b></span></a></th>
-			<th style="width: max-content;" ><a class="tooltip" href="#"><?php echo $lang['pocsag_msg'];?><span><b>Message contents</b></span></a></th>
-		    </tr>
-		</thread>
-	    </table>
-	</div>
-	
-	<div style="max-height:190px; overflow-y:auto;" >
-	    <table>
-		<thread>
-		    <tr>
-			<th></th>
-			<th></th>
-		    </tr>
-		</thread>
-		
-		<tbody>
+	    <b><?php echo $lang['pocsag_persolist'];?></b>
+	    <div>
+		<table>
+		    <thread>
+			<tr>
+			    <th style="width: 140px;" ><a class="tooltip" href="#"><?php echo $lang['time'];?> (<?php echo date('T')?>)<span><b>Time in <?php echo date('T')?> time zone</b></span></a></th>
+			    <th style="width: max-content;" ><a class="tooltip" href="#"><?php echo $lang['pocsag_msg'];?><span><b>Message contents</b></span></a></th>
+			</tr>
+		    </thread>
+		</table>
+	    </div>
+	    
+	    <div style="max-height:190px; overflow-y:auto;" >
+		<table>
+		    <thread>
+			<tr>
+			    <th></th>
+			    <th></th>
+			</tr>
+		    </thread>
 		    
-		    <?php
-		    
-		    $found = false;
-		    
-		    foreach ($logLinesDAPNETGateway as $dapnetMessageLine) {
+		    <tbody>
+			<?php
+			$found = false;
 			
-			// After this, only messages for my RIC are stored
-			if (!$found && strcmp($dapnetMessageLine, '<MY_RIC>') == 0) {
-			    $found = true;
-			    continue;
-			}
-			
-			if ($found) {
-			    $dapnetMessageArr = explode(" ", $dapnetMessageLine);
-			    $utc_time = $dapnetMessageArr["0"]." ".substr($dapnetMessageArr["1"],0,-4);
-			    $utc_tz = new DateTimeZone('UTC');
-			    $local_tz = new DateTimeZone(date_default_timezone_get ());
-			    $dt = new DateTime($utc_time, $utc_tz);
-			    $dt->setTimeZone($local_tz);
-			    $local_time = $dt->format('H:i:s M jS');
-			    
-			    $pos = strpos($dapnetMessageLine, '"');
-			    $len = strlen($dapnetMessageLine);
-			    $pocsag_msg = substr($dapnetMessageLine, ($pos - $len) + 1, ($len - $pos) - 2);
-			    
-			    // Formatting long messages without spaces
-			    if (strpos($pocsag_msg, ' ') == 0 && strlen($pocsag_msg) >= 70) {
-				$pocsag_msg = wordwrap($pocsag_msg, 70, ' ', true);
+			foreach ($logLinesDAPNETGateway as $dapnetMessageLine) {
+			    // After this, only messages for my RIC are stored
+			    if (!$found && strcmp($dapnetMessageLine, '<MY_RIC>') == 0) {
+				$found = true;
+				continue;
 			    }
 			    
-		    ?>
-		    
-		            <tr>
-				<td style="width: 140px; vertical-align: top; text-align: left;"><?php echo $local_time; ?></td>
-				<td style="width: max-content; vertical-align: top; text-align: left; word-wrap: break-word; white-space: normal !important;"><?php echo $pocsag_msg; ?></td>
-			    </tr>
-		    
-<?php
-                        } // $found
-                    } // foreach
-?>
-		</tbody>
-	    </table>
-	    
+			    if ($found) {
+				$dapnetMessageArr = explode(" ", $dapnetMessageLine);
+				$utc_time = $dapnetMessageArr["0"]." ".substr($dapnetMessageArr["1"],0,-4);
+				$utc_tz = new DateTimeZone('UTC');
+				$local_tz = new DateTimeZone(date_default_timezone_get ());
+				$dt = new DateTime($utc_time, $utc_tz);
+				$dt->setTimeZone($local_tz);
+				$local_time = $dt->format('H:i:s M jS');
+				
+				$pos = strpos($dapnetMessageLine, '"');
+				$len = strlen($dapnetMessageLine);
+				$pocsag_msg = substr($dapnetMessageLine, ($pos - $len) + 1, ($len - $pos) - 2);
+				
+				// Formatting long messages without spaces
+				if (strpos($pocsag_msg, ' ') == 0 && strlen($pocsag_msg) >= 70) {
+				    $pocsag_msg = wordwrap($pocsag_msg, 70, ' ', true);
+				}
+			?>
+		                <tr>
+				    <td style="width: 140px; vertical-align: top; text-align: left;"><?php echo $local_time; ?></td>
+				    <td style="width: max-content; vertical-align: top; text-align: left; word-wrap: break-word; white-space: normal !important;"><?php echo $pocsag_msg; ?></td>
+				</tr>
+                        <?php
+                            } // $found
+                        } // foreach
+			?>
+		    </tbody>
+		</table>
+	    </div>
 	</div>
-	
 	<br />
-	
+
 <?php
     } // $myRIC
+} // admin
 ?>
 
 <div>
-    
     <div>
-	
 	<!-- Activity -->
-	<b><?php echo $lang['pocsag_list'];?></b>
-	
+	<b><?php echo $lang['pocsag_list'];?></b>	
 	<div>
 	    <table>
 		<thread>
@@ -237,51 +224,24 @@ if (strcmp($origin, "admin") == 0) {
 	    </table>
 	</div>
 	
-	<div style="max-height:190px; overflow-y:auto;" >
+	<div style="max-height:198px; overflow-y:auto;" >
 	    <table>
 		<tbody>
-		    <?php listDAPNETGWMessages($logLinesDAPNETGateway, true); ?>
+		    <?php listDAPNETGWMessages($logLinesDAPNETGateway, ((strcmp($origin, "admin") == 0) ? true : false)); ?>
 		</tbody>
 	    </table>
-	    
 	</div>
-        
-    </div>
-    
-    <div style="float: right; vertical-align: bottom; padding-top: 5px;">
-	<div class="grid-container" style="display: inline-grid; grid-template-columns: auto 40px; padding: 1px; grid-column-gap: 5px;">
-	    <div class="grid-item" style="padding-top: 3px;" >POCSAG Auto Refresh
-	    </div>
-	    <div class="grid-item" >
-		<div> <input id="toggle-pocsag-autorefresh" class="toggle toggle-round-flat" type="checkbox" name="pocsag-autorefresh" value="ON" checked="checked" aria-checked="true" aria-label="POCSAG Auto Refresh" onchange="setPagesAutorefresh(this)" /><label for="toggle-pocsag-autorefresh" ></label>
+	<div style="display:inline-block;width: 100%;">
+	    <div style="float: right; vertical-align: bottom; padding-top: 5px;">
+		<div class="grid-container" style="display: inline-grid; grid-template-columns: auto 40px; padding: 1px; grid-column-gap: 5px;">
+		    <div class="grid-item" style="padding-top: 3px;" >Auto Refresh
+		    </div>
+		    <div class="grid-item" >
+			<div> <input id="toggle-pocsag-autorefresh" class="toggle toggle-round-flat" type="checkbox" name="pocsag-autorefresh" value="ON" checked="checked" aria-checked="true" aria-label="POCSAG Auto Refresh" onchange="setPagesAutorefresh(this)" /><label for="toggle-pocsag-autorefresh" ></label>
+			</div>
+		    </div>
 		</div>
 	    </div>
 	</div>
     </div>
-    
 </div>
-
-<?php
-
-}
-else { // origin == "admin"
-
-?>
-    
-    <b><?php echo $lang['pocsag_list'];?></b>
-    
-    <table>
-	<tr>
-	    <th><a class="tooltip" href="#"><?php echo $lang['time'];?> (<?php echo date('T')?>)<span><b>Time in <?php echo date('T')?> time zone</b></span></a></th>
-	    <th><a class="tooltip" href="#"><?php echo $lang['pocsag_timeslot'];?><span><b>Message Mode</b></span></a></th>
-	    <th><a class="tooltip" href="#"><?php echo $lang['target'];?><span><b>RIC / CapCode of the receiving Pager</b></span></a></th>
-	    <th><a class="tooltip" href="#"><?php echo $lang['pocsag_msg'];?><span><b>Message contents</b></span></a></th>
-	</tr>
-	
-	<?php listDAPNETGWMessages($logLinesDAPNETGateway, false); ?>
-	
-    </table>
-    
-<?php
-} // origin == "admin"
-?>
