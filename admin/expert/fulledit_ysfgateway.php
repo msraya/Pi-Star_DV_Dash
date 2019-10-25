5b16f513f93f88019393b7deb25fdf10435fff41
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2019-10-25 08:52:34 +0200

    Add missing ysfgateway full editor.

---
+++ admin/expert/fulledit_ysfgateway.php
@@ -0,0 +1,9 @@
+<?php
+
+$configfile = '/etc/ysfgateway';
+$tempfile = '/tmp/eXNmZ2F0ZXdheQ.tmp';
+$servicenames = array('mmdvmhost.service', 'ysfgateway.service');
+
+require_once('fulledit_template.php');
+
+?>
