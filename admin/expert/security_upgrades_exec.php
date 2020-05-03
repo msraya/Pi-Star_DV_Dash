0502ce452a3fe1da21cebed2ab78760af7027721
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2020-05-03 09:34:33 +0200

    Update package repo before calling unattented-upgrades.

--- admin/expert/security_upgrades_exec.php
+++ admin/expert/security_upgrades_exec.php
@@ -4,6 +4,7 @@ $cmdoutput = array();
 exec('systemctl stop pistar-watchdog.timer > /dev/null 2>&1');
 exec('systemctl stop pistar-watchdog.service > /dev/null 2>&1');
 exec('sudo mount -o remount,rw /');
+exec('sudo apt-get update > /dev/null 2<&1');
 $cmdresult = exec('sudo /usr/bin/unattended-upgrade > /dev/null 2<&1', $cmdoutput, $retvalue);
 exec('sudo mount -o remount,ro /');
 exec('systemctl start pistar-watchdog.service > /dev/null 2>&1');
